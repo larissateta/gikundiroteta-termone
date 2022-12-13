f0403abf8accfb4177dde9f30dd5f406ae7e2de1
larissateta <84853862+larissateta@users.noreply.github.com>
2022-12-13 08:57:50 +0200

    first commit

---
+++ myCalculator_backend/src/main/java/com/example/mycalculator_backend/MyCalculatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycalculator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCalculatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCalculatorBackendApplication.class, args);
+	}
+
+}
---
+++ myCalculator_backend/src/test/java/com/example/mycalculator_backend/MyCalculatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mycalculator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCalculatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
